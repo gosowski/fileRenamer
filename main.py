8d1fe85d98592b82616a3f6f835d3360ad56b8d6
gosowski <osowski.grzegorz@gmail.com>
2018-05-29 14:18:29 +0200

    removed commented lines

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 import os
 import sys
 
-# argNumber = len(sys.argv)
-
-# if argNumber >= 2:
-#   newName = sys.argv[2] + "_"
-#   counter = int(sys.argv[3])
-#   path = sys.argv[1]
-#   names = os.listdir(path)
-  
-#   for filename in names:
-#     os.rename(path+'/'+filename, path+'/'+newName+str(counter))
-#     counter+=1
-
-# else:
-#   print("Bye!")
-
 try:
   oldNames = os.listdir(sys.argv[1])
 
