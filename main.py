a836857b27c9f5dbb43339d2bfd16ed61cbda60a
gosowski <osowski.grzegorz@gmail.com>
2018-05-29 13:59:34 +0200

    added IndexError exception

--- main.py
+++ main.py
@@ -20,21 +20,23 @@ try:
   oldNames = os.listdir(sys.argv[1])
 
   if not os.listdir(sys.argv[1]):
-    sys.exit(sys.argv[1]+" is empty")
+    sys.exit(sys.argv[1]+" is empty")  
   
-  else:
-    newName = sys.argv[2] + "_"
-    counter = int(sys.argv[3])
-    path = sys.argv[1]
+  newName = sys.argv[2] + "_"
+  counter = int(sys.argv[3])
+  path = sys.argv[1]
 
-    for filename in oldNames:
-      os.rename(path+'/'+filename, path+'/'+newName+str(counter))
-      counter+=1
+  for filename in oldNames:
+    os.rename(path+'/'+filename, path+'/'+newName+str(counter))
+    counter+=1
 
-    print("Done")
+  print("Done")
 
 except OSError:
-  print("Cannot open ", sys.argv[1])
+  print("Cannot open "+sys.argv[1])
+
+except IndexError:
+  print("Incompleted argument list")
 
 
 
